3df0c96256e22f9f5ff06b3de51598c2a9a8601c
Simon Kern <skern@gleason.com>
2022-07-17 20:59:35 +0200

    swipe for clock

--- apps/swabianfuzzy/swabianfuzzy.app.js
+++ apps/swabianfuzzy/swabianfuzzy.app.js
@@ -22,7 +22,7 @@ let fuzzy_string = {
     "viertl *$2",
     "zwanzg nache *$1",
     "fünf vor halbe *$2",
-    "halb *$2",
+    "halbe *$2",
     "fünf nach halbe *$2",
     "zea nach halbe *$2",
     "dreiviertl *$2",
@@ -31,15 +31,16 @@ let fuzzy_string = {
   ]
 };
 img = img = {
-  width : 100, height : 100, bpp : 3,
-  transparent : 2,
-  buffer : require("heatshrink").decompress(atob("pMkyQCQysf//FC6YCMpUP///wgjeAQOHEgJKhoYkBJUMlEgRKhjZKjkIkC4RKfyAkC/iSeIgNRJT8IkEAhEQJT1RosUAQMJh4lC6RKZgGAgEAgekSofhkE6EalIqMEiBHBjgICv/Aj/xoEAI6kon///04SQILC2H4OIJSBgRKUj5mC//HgDgBJQfwgEdJSkBEgYAB/EAsmw/kPAoJTDJS4AB+PAF4PA7kAgsQoMFhImRkCVBAAuAiEAhyUBAAhKQysQv4lGgBKBEg0BoQmOpQTBEg3+EQwqDghKNktFilB46VE48cEYkcvv3BASYOwAYCh7gDgYJDAAcb9u+8EAiRKMqJKBuPAjiYCcAKPE4EEgEH79940VJRmAgUAk3b90AgeOnAjDhzsBx++gEN23b9BuMiEB88fQwPcJoJqEg/8KYKVB44RB45uMkM0idtHAICC9glEv/4TwX7t4OBgBKLyseuJKDQwP3JQqbBTwQODihKLQAPToJKEtgkDgboCAAMAJQVokRKKwueJQ/cboYjDj7pBCIUUqhKJgEJm3Dh5KEtxKD8YkC/0/8fwmwOBkGJJRFR48QvPHI4SVFjl/44kB44FB//8LIVRqRKHyBEBgnD9pKEsAkBh5ICEwOP/5QB/Eb9uggJKIpPnzlhI4ZKFEgX8JoMcuIGB+EHJQNCJRW0jZHCAQWAJQoAG/kP3EAgiVI87gBJQlx4AkBg5KF//xTYTmBuNFJREAhu24ZKEEYXgJRTmBm0AhBKIoseJQtwEoLdDNYjgBAoZKBhJKG0ECgM2JQiDBgE/bQQAK/ARBJQ1IJQMYJQ7XBJQwAG+NFipKHSoMAboZKCh/4JRv/8EAiRKHo+cI4QCB7jdC+PAEhn/4hKHPINN0xKDty5CCwJKN/+AnRKFqhKG44kBg7RB4EfEpsUJQ8Cpk2SQUDpsggYUB8eP/AFCABP+gRKHokfJQMYvPnzxHBcAUAAQJNL/l0JQ0D96SBxJNCAoNAg5KB45wN4GAogmDiiVBSQWevPHjxNBJQP/jkBEhhWB48JJQUoJQPt23DI4PDhpKBtuw//x/BKNAAPwiRKEu/fuKSBSoSYDHYPxEp8RJQ2wSQYCE4f+n4iNd4P+kRKE+/cI4oCD48Da4pTBcAzvB/hKF90m7dsn/zJQnQhxKE/0A8F/Ew3jJQ0AiBEBZAJHDAQPAjgdEBwIAJgJKEYQM27cPGQJKCcYUB44YDx4kK/zgEvv3jxEBBgPHAoPETARKEFIiVHhJKClSGBxLXBPoQQBJQdgnAaDSQ7gCgEEEYImBj/9+0eZwXxDQKVD88AgJKDcBAXB6QjCpMhBAON22f/zXCJQKYCtkAWwRKB/BLI9IjCJQV9III4BgBiBJQLjCJQJEDSpLXBEYZKEmgLBJRE2TwIJBd4IAIXIZKEjg+BVoRKFzAOBDIPAOgRKMl0/HYXjxxKBx02JQkm7DdCwAUDAAQFB/AjDAQNUuP3799HYXHjxKJUgLmDAAn0JQkkwAKC/kAn/8hJHBSo4AIJRFIvpKBv/3jl/cwJKEyChBBYIkH+I+BEYgCBw4PE8HjI4RKE4f+dgQAI8RKI7hNCuJHDAQUAdghKI/pKM/8TJQton0f/0HJJP+EYqVEHwLjBSo0fSpYABhJKKgaJB/lNJQUA6dB2xKBgAkJwhKHboJKB48cJQmYBAJKDAQIAH4QjFAQOQBgXj/04/+NSQU/8EP23fJQPjEg/hEYwCBqJKDiabBjySC/vHUITgB464G+AjHAQMDBwPigO/eQO27UPBIOP2xHCKAIkFgGJJREhg/fIIMfJQVxw7mB+gIBUINB+8AcYfHgFUJRNAeovx2EbAoWBTYPun3bt0AL4P98EAkQjIAQOGRgKYBJQLjBAoZTCAoXcuEAgFBosVEZICBSQQACxMHKAhKBtoCD/fuEwMAiRKKyUQHwf2JRYCCilRosJJRckwBEDhkAAobgBJQvb4BJBggjLcYnxa4MBIIXG79x45KE4pKBoQjMAQNKQQUP3EfJIP8AoOTgO+/fv3xJCgIjNAQOXQYUfIgVx4EYvPnjlh4wCBosUqgjOAQRLCk3gI4MApswSQMG4aVBwEAkQjQyVIJQJNBTAN94+euPnzl5AoNwgsVEaACByg7BAAMJn3jtu0wdt2Hbhu24DdOAQslJQRNEuNHzlJ8wFBbp4CGhBLCgECAQMDJQQFBxIjUAQNUJQYCCo+epPnigjWAQSYDAAOAppKBhwjYAQOVJQlEjlB4ojZAQWQwUAkACBgVCEbYC/AX4C/AX4CdA"))
+  width: 100, height: 100, bpp: 3,
+  transparent: 2,
+  buffer: require("heatshrink").decompress(atob("pMkyQCQysf//FC6YCMpUP///wgjeAQOHEgJKhoYkBJUMlEgRKhjZKjkIkC4RKfyAkC/iSeIgNRJT8IkEAhEQJT1RosUAQMJh4lC6RKZgGAgEAgekSofhkE6EalIqMEiBHBjgICv/Aj/xoEAI6kon///04SQILC2H4OIJSBgRKUj5mC//HgDgBJQfwgEdJSkBEgYAB/EAsmw/kPAoJTDJS4AB+PAF4PA7kAgsQoMFhImRkCVBAAuAiEAhyUBAAhKQysQv4lGgBKBEg0BoQmOpQTBEg3+EQwqDghKNktFilB46VE48cEYkcvv3BASYOwAYCh7gDgYJDAAcb9u+8EAiRKMqJKBuPAjiYCcAKPE4EEgEH79940VJRmAgUAk3b90AgeOnAjDhzsBx++gEN23b9BuMiEB88fQwPcJoJqEg/8KYKVB44RB45uMkM0idtHAICC9glEv/4TwX7t4OBgBKLyseuJKDQwP3JQqbBTwQODihKLQAPToJKEtgkDgboCAAMAJQVokRKKwueJQ/cboYjDj7pBCIUUqhKJgEJm3Dh5KEtxKD8YkC/0/8fwmwOBkGJJRFR48QvPHI4SVFjl/44kB44FB//8LIVRqRKHyBEBgnD9pKEsAkBh5ICEwOP/5QB/Eb9uggJKIpPnzlhI4ZKFEgX8JoMcuIGB+EHJQNCJRW0jZHCAQWAJQoAG/kP3EAgiVI87gBJQlx4AkBg5KF//xTYTmBuNFJREAhu24ZKEEYXgJRTmBm0AhBKIoseJQtwEoLdDNYjgBAoZKBhJKG0ECgM2JQiDBgE/bQQAK/ARBJQ1IJQMYJQ7XBJQwAG+NFipKHSoMAboZKCh/4JRv/8EAiRKHo+cI4QCB7jdC+PAEhn/4hKHPINN0xKDty5CCwJKN/+AnRKFqhKG44kBg7RB4EfEpsUJQ8Cpk2SQUDpsggYUB8eP/AFCABP+gRKHokfJQMYvPnzxHBcAUAAQJNL/l0JQ0D96SBxJNCAoNAg5KB45wN4GAogmDiiVBSQWevPHjxNBJQP/jkBEhhWB48JJQUoJQPt23DI4PDhpKBtuw//x/BKNAAPwiRKEu/fuKSBSoSYDHYPxEp8RJQ2wSQYCE4f+n4iNd4P+kRKE+/cI4oCD48Da4pTBcAzvB/hKF90m7dsn/zJQnQhxKE/0A8F/Ew3jJQ0AiBEBZAJHDAQPAjgdEBwIAJgJKEYQM27cPGQJKCcYUB44YDx4kK/zgEvv3jxEBBgPHAoPETARKEFIiVHhJKClSGBxLXBPoQQBJQdgnAaDSQ7gCgEEEYImBj/9+0eZwXxDQKVD88AgJKDcBAXB6QjCpMhBAON22f/zXCJQKYCtkAWwRKB/BLI9IjCJQV9III4BgBiBJQLjCJQJEDSpLXBEYZKEmgLBJRE2TwIJBd4IAIXIZKEjg+BVoRKFzAOBDIPAOgRKMl0/HYXjxxKBx02JQkm7DdCwAUDAAQFB/AjDAQNUuP3799HYXHjxKJUgLmDAAn0JQkkwAKC/kAn/8hJHBSo4AIJRFIvpKBv/3jl/cwJKEyChBBYIkH+I+BEYgCBw4PE8HjI4RKE4f+dgQAI8RKI7hNCuJHDAQUAdghKI/pKM/8TJQton0f/0HJJP+EYqVEHwLjBSo0fSpYABhJKKgaJB/lNJQUA6dB2xKBgAkJwhKHboJKB48cJQmYBAJKDAQIAH4QjFAQOQBgXj/04/+NSQU/8EP23fJQPjEg/hEYwCBqJKDiabBjySC/vHUITgB464G+AjHAQMDBwPigO/eQO27UPBIOP2xHCKAIkFgGJJREhg/fIIMfJQVxw7mB+gIBUINB+8AcYfHgFUJRNAeovx2EbAoWBTYPun3bt0AL4P98EAkQjIAQOGRgKYBJQLjBAoZTCAoXcuEAgFBosVEZICBSQQACxMHKAhKBtoCD/fuEwMAiRKKyUQHwf2JRYCCilRosJJRckwBEDhkAAobgBJQvb4BJBggjLcYnxa4MBIIXG79x45KE4pKBoQjMAQNKQQUP3EfJIP8AoOTgO+/fv3xJCgIjNAQOXQYUfIgVx4EYvPnjlh4wCBosUqgjOAQRLCk3gI4MApswSQMG4aVBwEAkQjQyVIJQJNBTAN94+euPnzl5AoNwgsVEaACByg7BAAMJn3jtu0wdt2Hbhu24DdOAQslJQRNEuNHzlJ8wFBbp4CGhBLCgECAQMDJQQFBxIjUAQNUJQYCCo+epPnigjWAQSYDAAOAppKBhwjYAQOVJQlEjlB4ojZAQWQwUAkACBgVCEbYC/AX4C/AX4CdA"))
 };
 
 let text_scale = 3.5;
 let timeout_unlocked = 1;
 let timeout_locked = 2.5 * 60;
 let drawTimeout;
+let screen = "fuzzy";
 
 function queueDraw(seconds) {
   let millisecs = seconds * 1000;
@@ -53,7 +54,7 @@ function queueDraw(seconds) {
 const h = g.getHeight();
 const w = g.getWidth();
 
-function getTimeString(date) {
+function getFuzzyTimeString(date) {
   let segment = Math.round((date.getMinutes() * 60 + date.getSeconds() + 1) / 300);
   let hour = date.getHours() + Math.floor(segment / 12);
   f_string = fuzzy_string.minutes[segment % 12];
@@ -65,31 +66,40 @@ function getTimeString(date) {
   return f_string;
 }
 
-function getDetailString(date) {
+function getTimeString(date) {
   let da = date.toString().split(" ");
   let time = da[4].split(":");
   let hours = time[0], minutes = time[1], seconds = time[2];
-  let datestr = da[2] + '. ' + da[1];
-  return `${hours}:${minutes}:${seconds} - ${datestr}`;
+  return `${hours}:${minutes}:${seconds}`;
+}
+
+function getDateString(date) {
+  let da = date.toString().split(" ");
+  return `${da[0]} ${da[2]}. ${da[1]} ${da[3]}`;
 }
 
 function draw() {
-  g.drawImage(img, 30, 10);
   let date = new Date();
-  let time_string = getTimeString(date).replace('*', '');
-  // print(time_string);
-  g.setFont('Vector', (h - 24 * 2) / text_scale);
-  g.setFontAlign(0, 0);
+
   g.clearRect(0, 24, w, h);
   g.setColor(g.theme.fg);
+  g.setFontAlign(0, 0);
   g.drawImage(img, 40, 0);
-  g.drawString(g.wrapString(time_string, w).join("\n"), w / 2, h / 2 + 30);
-  if (!Bangle.isLocked()) {
-    g.setFont("12x20", 1);
-    g.drawString(getDetailString(date), w / 2, h - 10, true);
-    queueDraw(timeout_unlocked);
-  } else {
-    queueDraw(timeout_locked);
+
+  switch (screen) {
+    case "fuzzy":
+      let time_string = getFuzzyTimeString(date).replace('*', '');
+      g.setFont('Vector', 30);
+      g.drawString(g.wrapString(time_string, w).join("\n"), w / 2, h / 2 + 40);
+      queueDraw(timeout_locked);
+      break;
+    case "clock":
+      g.setFont('Vector', 30);
+      g.drawString(getTimeString(date), w / 2, h / 2 + 30);
+      g.setFont('Vector', 15);
+      g.drawString(getDateString(date), w / 2, h / 2 + 60);
+      queueDraw(timeout_unlocked);
+      break;
   }
 }
 
@@ -106,7 +116,20 @@ Bangle.on('lcdPower', on => {
   }
 });
 Bangle.on('lock', on => {
-  draw(); // draw immediately, queue redraw
+  screen = "fuzzy";
+  queueDraw(1); // draw after widgets
+});
+
+Bangle.on('swipe', function (direction) {
+  switch (screen) {
+    case "fuzzy":
+      screen = "clock";
+      break;
+    case "clock":
+      screen = "fuzzy";
+      break;
+  }
+  draw();
 });
 
 Bangle.setUI('clock');
