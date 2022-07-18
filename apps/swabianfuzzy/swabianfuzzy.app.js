98c94123bcb53593c5241c597aa7c5bc81a1e900
Simon Kern <skern@gleason.com>
2022-07-18 19:15:38 +0200

    added steps and battery

--- apps/swabianfuzzy/swabianfuzzy.app.js
+++ apps/swabianfuzzy/swabianfuzzy.app.js
@@ -36,9 +36,10 @@ img = img = {
   buffer: require("heatshrink").decompress(atob("pMkyQCQysf//FC6YCMpUP///wgjeAQOHEgJKhoYkBJUMlEgRKhjZKjkIkC4RKfyAkC/iSeIgNRJT8IkEAhEQJT1RosUAQMJh4lC6RKZgGAgEAgekSofhkE6EalIqMEiBHBjgICv/Aj/xoEAI6kon///04SQILC2H4OIJSBgRKUj5mC//HgDgBJQfwgEdJSkBEgYAB/EAsmw/kPAoJTDJS4AB+PAF4PA7kAgsQoMFhImRkCVBAAuAiEAhyUBAAhKQysQv4lGgBKBEg0BoQmOpQTBEg3+EQwqDghKNktFilB46VE48cEYkcvv3BASYOwAYCh7gDgYJDAAcb9u+8EAiRKMqJKBuPAjiYCcAKPE4EEgEH79940VJRmAgUAk3b90AgeOnAjDhzsBx++gEN23b9BuMiEB88fQwPcJoJqEg/8KYKVB44RB45uMkM0idtHAICC9glEv/4TwX7t4OBgBKLyseuJKDQwP3JQqbBTwQODihKLQAPToJKEtgkDgboCAAMAJQVokRKKwueJQ/cboYjDj7pBCIUUqhKJgEJm3Dh5KEtxKD8YkC/0/8fwmwOBkGJJRFR48QvPHI4SVFjl/44kB44FB//8LIVRqRKHyBEBgnD9pKEsAkBh5ICEwOP/5QB/Eb9uggJKIpPnzlhI4ZKFEgX8JoMcuIGB+EHJQNCJRW0jZHCAQWAJQoAG/kP3EAgiVI87gBJQlx4AkBg5KF//xTYTmBuNFJREAhu24ZKEEYXgJRTmBm0AhBKIoseJQtwEoLdDNYjgBAoZKBhJKG0ECgM2JQiDBgE/bQQAK/ARBJQ1IJQMYJQ7XBJQwAG+NFipKHSoMAboZKCh/4JRv/8EAiRKHo+cI4QCB7jdC+PAEhn/4hKHPINN0xKDty5CCwJKN/+AnRKFqhKG44kBg7RB4EfEpsUJQ8Cpk2SQUDpsggYUB8eP/AFCABP+gRKHokfJQMYvPnzxHBcAUAAQJNL/l0JQ0D96SBxJNCAoNAg5KB45wN4GAogmDiiVBSQWevPHjxNBJQP/jkBEhhWB48JJQUoJQPt23DI4PDhpKBtuw//x/BKNAAPwiRKEu/fuKSBSoSYDHYPxEp8RJQ2wSQYCE4f+n4iNd4P+kRKE+/cI4oCD48Da4pTBcAzvB/hKF90m7dsn/zJQnQhxKE/0A8F/Ew3jJQ0AiBEBZAJHDAQPAjgdEBwIAJgJKEYQM27cPGQJKCcYUB44YDx4kK/zgEvv3jxEBBgPHAoPETARKEFIiVHhJKClSGBxLXBPoQQBJQdgnAaDSQ7gCgEEEYImBj/9+0eZwXxDQKVD88AgJKDcBAXB6QjCpMhBAON22f/zXCJQKYCtkAWwRKB/BLI9IjCJQV9III4BgBiBJQLjCJQJEDSpLXBEYZKEmgLBJRE2TwIJBd4IAIXIZKEjg+BVoRKFzAOBDIPAOgRKMl0/HYXjxxKBx02JQkm7DdCwAUDAAQFB/AjDAQNUuP3799HYXHjxKJUgLmDAAn0JQkkwAKC/kAn/8hJHBSo4AIJRFIvpKBv/3jl/cwJKEyChBBYIkH+I+BEYgCBw4PE8HjI4RKE4f+dgQAI8RKI7hNCuJHDAQUAdghKI/pKM/8TJQton0f/0HJJP+EYqVEHwLjBSo0fSpYABhJKKgaJB/lNJQUA6dB2xKBgAkJwhKHboJKB48cJQmYBAJKDAQIAH4QjFAQOQBgXj/04/+NSQU/8EP23fJQPjEg/hEYwCBqJKDiabBjySC/vHUITgB464G+AjHAQMDBwPigO/eQO27UPBIOP2xHCKAIkFgGJJREhg/fIIMfJQVxw7mB+gIBUINB+8AcYfHgFUJRNAeovx2EbAoWBTYPun3bt0AL4P98EAkQjIAQOGRgKYBJQLjBAoZTCAoXcuEAgFBosVEZICBSQQACxMHKAhKBtoCD/fuEwMAiRKKyUQHwf2JRYCCilRosJJRckwBEDhkAAobgBJQvb4BJBggjLcYnxa4MBIIXG79x45KE4pKBoQjMAQNKQQUP3EfJIP8AoOTgO+/fv3xJCgIjNAQOXQYUfIgVx4EYvPnjlh4wCBosUqgjOAQRLCk3gI4MApswSQMG4aVBwEAkQjQyVIJQJNBTAN94+euPnzl5AoNwgsVEaACByg7BAAMJn3jtu0wdt2Hbhu24DdOAQslJQRNEuNHzlJ8wFBbp4CGhBLCgECAQMDJQQFBxIjUAQNUJQYCCo+epPnigjWAQSYDAAOAppKBhwjYAQOVJQlEjlB4ojZAQWQwUAkACBgVCEbYC/AX4C/AX4CdA"))
 };
 
-let text_scale = 3.5;
-let timeout_unlocked = 1;
-let timeout_locked = 2.5 * 60;
+let timeout_clock = 1;
+let timeout_fuzzy = 2.5 * 60;
+let timeout_steps = 2;
+let timeout_battery = 2.5 * 60;
 let drawTimeout;
 let screen = "fuzzy";
 
@@ -91,20 +92,35 @@ function draw() {
       let time_string = getFuzzyTimeString(date).replace('*', '');
       g.setFont('Vector', 30);
       g.drawString(g.wrapString(time_string, w).join("\n"), w / 2, h / 2 + 40);
-      queueDraw(timeout_locked);
+      queueDraw(timeout_fuzzy);
       break;
     case "clock":
       g.setFont('Vector', 30);
       g.drawString(getTimeString(date), w / 2, h / 2 + 30);
       g.setFont('Vector', 15);
       g.drawString(getDateString(date), w / 2, h / 2 + 60);
-      queueDraw(timeout_unlocked);
+      queueDraw(timeout_clock);
+      break;
+    case "steps":
+      g.setFont('Vector', 30);
+      g.drawString(Bangle.getStepCount(), w / 2, h / 2 + 30);
+      g.setFont('Vector', 15);
+      g.drawString("Schritte", w / 2, h / 2 + 60);
+      queueDraw(timeout_steps);
+      break;
+    case "battery":
+      g.setFont('Vector', 30);
+      g.drawString(`${E.getBattery()}%`, w / 2, h / 2 + 30);
+      g.setFont('Vector', 15);
+      g.drawString("Batterie", w / 2, h / 2 + 60);
+      queueDraw(timeout_battery);
       break;
   }
 }
 
 g.clear();
 draw();
+queueDraw(1); // draw after widgets
 
 // Stop updates when LCD is off, restart when on
 Bangle.on('lcdPower', on => {
@@ -120,14 +136,16 @@ Bangle.on('lock', on => {
   queueDraw(1); // draw after widgets
 });
 
-Bangle.on('swipe', function (direction) {
-  switch (screen) {
-    case "fuzzy":
-      screen = "clock";
-      break;
-    case "clock":
-      screen = "fuzzy";
-      break;
+
+screens = ["fuzzy", "clock", "steps", "battery"];
+
+Bangle.on('swipe', function (directionLR, directionUD) {
+  if (directionLR + directionUD > 0) {
+    screen = screens.shift();
+    screens.push(screen);
+  } else {
+    screen = screens.pop();
+    screens.unshift(screen);
   }
   draw();
 });
